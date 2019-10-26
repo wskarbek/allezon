ef259eb68b42e898959018f0910888628e8ffa76
wskarbek <ktsulv@gmail.com>
2019-10-26 17:55:26 +0200

    full name in registration form is now divided into name and surname account class created changes to css

--- app/src/main/java/pl/edu/pjwstk/jazapp/register/RegisterRequest.java
+++ app/src/main/java/pl/edu/pjwstk/jazapp/register/RegisterRequest.java
@@ -6,20 +6,23 @@ import javax.inject.Named;
 @Named
 @RequestScoped
 public class RegisterRequest {
-    private String username;
-    private String password;
-    private String email;
-    private String fullName;
-    private String passwordCheck;
+    private String username, password, passwordCheck, name, surname, email;
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
+
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getName() { return name; }
+    public void setName(String fullName) { this.name = fullName; }
+
+    public String getSurname() { return surname; }
+    public void setSurname(String surname) { this.surname = surname; }
+
     public String getPasswordCheck() { return passwordCheck; }
     public void setPasswordCheck(String passwordCheck) { this.passwordCheck = passwordCheck; }
 
@@ -29,7 +32,8 @@ public class RegisterRequest {
                 "username='" + username + '\'' +
                 ", password='" + password + '\'' +
                 ", passwordCheck='" + passwordCheck + '\'' +
-                ", fullName='" + fullName + '\'' +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
                 ", email='" + email + '\'' +
                 ",}";
     }
---
+++ app/src/main/java/pl/edu/pjwstk/jazapp/webapp/Account.java
@@ -0,0 +1,5 @@
+package pl.edu.pjwstk.jazapp.webapp;
+
+public class Account {
+
+}
